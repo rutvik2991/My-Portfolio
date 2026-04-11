5206d5ab602141b6a16425b36bb4f6b8410c6ad3
codeXzar <codexzar@gmail.com>
2026-04-11 12:19:35 +0530

    Updates

--- assets/Navbar-C5X0-66p.js
+++ assets/Navbar-C5X0-66p.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-BofDjON2.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-Dtyb_CJ-.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
